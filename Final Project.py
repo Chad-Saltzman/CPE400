367d6455b255bff9bf06d89d18518b2120a839ee
Chad-Saltzman <80727704+Chad-Saltzman@users.noreply.github.com>
2021-11-24 23:26:35 -0800

    Moved Routing Protocol class and Graph class to seperate files

--- Final Project.py
+++ Final Project.py
@@ -1,124 +1,26 @@
 
 
-class RoutingProtocol:
-    def __init__(self, graph):
-        self.graph = graph 
-    
-    def RIP(self, source, destination, path = []):
-        path = path + [source]
-        if source == destination:
-            return path
-        shortest = None
-        for node in self.graph[source]:
-            if node not in path:
-                newpath = self.RIP(node, destination, path)
-                if newpath:
-                    if not shortest or len(newpath) < len(shortest):
-                        shortest = newpath
-        return shortest
-
-    def OSPF(self, source, destination):
-        pass 
-
-
+from RoutingProtocol import *
+from Graph import *
         
-
-class Graph:
-
-    def __init__(self, graphdict = {}):
-        self.graph = graphdict 
-        self.edges = self.generate_edges()
-
-    def generate_edges(self):
-        edges = []
-
-        for node in self.graph:
-            for neighbour in self.graph[node]:
-                if neighbour:
-                    edges.append((node, neighbour))
-        return edges 
-    
-    def __str__(self):
-        output = ""
-        for node in self.graph:
-            Connections = node + " -> "
-            for neighbor in self.graph[node]:
-                Connections = Connections + neighbor + " -> "
-            output = output + '\n' + Connections.strip().strip('->')
-        return output.lstrip('\n')
-
-    def getGraph(self):
-        userinput = ""
-        print("Enter in your graph in the following structure: <node>,<Connected to 1>, <Connected to 2> ... <Enter>")
-        print("When you have completed entering all the nodes and connections, type done and press enter")
-        currentNode = ""  
-        while currentNode != "done":
-            userinput = input()
-            userinput = userinput.split(",")
-            currentNode = userinput[0]
-            self.graph[userinput[0]] = []
-            
-            userinput.pop(0)
-            for node in userinput:
-                self.graph[currentNode].append(node)
-        del self.graph["done"]
-
-    def getNodes(self):
-        nodes = []
-        for node in self.graph:
-            nodes.append(node)
-            
-        return nodes
-
-    def getSourceAndDestinationNodes(self):
-        nodes = self.getNodes()
-        source = ""
-        destination = ""
-        print("Select the source node from the following: ")
-
-        while source not in nodes:
-            print(nodes)
-            source = input()
-            if source not in nodes:
-                print("Invalid input")
-                print("Select the source node from the following: ")
-
-        print("\nSelect the destination node from the following: ")
-
-        while destination not in nodes:
-            print(nodes)
-            destination = input()
-            if destination not in nodes:
-                print("Invalid input")
-                print("Select the destination node from the following: ")
-
-        return source, destination
-        
-
-            
-
-        
-
 sampleGraph = {
-    'A':['B'],
-    'B':['A','C','D'],
-    'C':['B','E'],
-    'D':['B','E','F','G'],
-    'E':['C','D','H'],
-    'F':['D','I','J'],
-    'G':['D','J'],
-    'H':['E'],
-    'I':['F','K'],
-    'J':['G','F'],
-    'K':['I','L','M'],
-    'L':['K','M','N','O','P'],
-    'M':['K','L','P'],
-    'N':['L','O'],
-    'O':['L','N','P'],
-    'P':['L','M','O'],
+    'A':{'B':5},
+    'B':{'A':5,'C':4,'D':2},
+    'C':{'B':4,'E':2},
+    'D':{'B':2,'E':5,'F':1,'G':4},
+    'E':{'C':2,'D':5,'H':5},
+    'F':{'D':1,'I':1,'J':3},
+    'G':{'D':4,'J':5},
+    'H':{'E':5},
+    'I':{'F':1,'K':2},
+    'J':{'G':5,'F':3},
+    'K':{'I':2,'L':1,'M':4},
+    'L':{'K':1,'M':2,'N':4,'O':1,'P':5},
+    'M':{'K':4,'L':2,'P':2},
+    'N':{'L':4,'O':2},
+    'O':{'L':1,'N':2,'P':3},
+    'P':{'L':5,'M':2,'O':3},
 }
-
-
 
 def main():
 
@@ -138,7 +40,7 @@ def main():
         print("The inserted graph is demonstrated below")
 
     else:
-        while Choice.upper != "Y" or Choice.upper != "N":
+        while Choice.upper() != "Y" or Choice.upper() != "N":
             Choice = input("Invalid input\nWould you like to use a predefined graph? Y/N: ")
 
     print(Network)
@@ -163,7 +65,6 @@ def main():
         shortest_path = FindRoute.OSPF(source = source, destination = destination)
     print(f"The shortest path from {source} to {destination} is:")
     print(shortest_path)
-
 
 if __name__ == '__main__':
     main()
